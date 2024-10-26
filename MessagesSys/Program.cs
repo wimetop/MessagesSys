326191cdeebb542bf238beb9bac5c50c052d08dc
Danylo <wimetop@gmail.com>
2024-10-26 10:03:08 +0300

    changed some code

--- MessagesSys/Program.cs
+++ MessagesSys/Program.cs
@@ -4,15 +4,12 @@ Store store = new();
 Customer oleg = new("Олег");
 Customer victoria = new("Вікторія");
 
-// Підписка клієнтів на категорії
 store.SubscribeToCategory("Смартфони", oleg);
 store.SubscribeToCategory("Ноутбуки", oleg);
 store.SubscribeToCategory("Ноутбуки", victoria);
 
-// Сповіщення про знижку на ноутбуки (отримають Олег і Вікторія)
 store.NotifyDiscounts("Ноутбуки", "Знижка 15%");
 
-// Олег відписується від категорії "Ноутбуки"
 store.UnsubscribeFromCategory("Ноутбуки", oleg);
 
 store.NotifyDiscounts("Смартфони", "Знижка 10%");
